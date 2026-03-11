4a7bc478aeb109c7411e02929b64c9d6d2160b08
Netik Agrawal <netik2897@gmail.com>
2026-03-12 01:24:14 +0530

    Security update for production

--- server/agrawal_store_backend/settings.py
+++ server/agrawal_store_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#yg%n07tp1)8!z#&66xb9flop1)zv0l013#ki!=gdww$b_qx)i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*'] # Added '*' temporarily for deployment setup
 
