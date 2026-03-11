efccd71be2fb23566205b7d589c8654290581ca7
Netik Agrawal <netik2897@gmail.com>
2026-03-11 23:59:32 +0530

    Deployment readiness: added STATIC_ROOT and updated requirements.txt

--- server/agrawal_store_backend/settings.py
+++ server/agrawal_store_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#yg%n07tp1)8!z#&66xb9flop1)zv0l013#ki!=gdww$b_qx)i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*'] # Added '*' temporarily for deployment setup
 
 
 # Application definition
@@ -144,6 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (Uploaded images)
 MEDIA_URL = '/media/'
